50d54f00f1f2bdf5551e54068a6468fa064e3591
mrlnsmr <marlen.schumer@web.de>
2023-08-28 16:50:24 +0200

    fixing code

--- Taschenrechner.py
+++ Taschenrechner.py
@@ -1,23 +1,28 @@
-#Rechenart
-rechenart = str(input("Welche Rechenart möchtest du verwenden? :"))
+from time import *
 
-#Zahlen
-zahl1 = int(input("Gib die erste Zahl ein mit der du Rechnen möchtest? :"))
+for i in range(100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000):
+    #Rechenart
+    rechenart = str(input('Welche Rechenart möchtest du verwenden? :'))
 
-zahl2 = int(input("Gib die zweite Zahl ein mit der du Rechnen möchtest? :"))
+    #Zahlen
+    zahl1 = int(input('Gib die erste Zahl ein mit der du Rechnen möchtest? :'))
+    zahl2 = int(input('Gib die zweite Zahl ein mit der du Rechnen möchtest? :'))
 
-#Addition
-if rechenart == "+" ():
-    print(f"{zahl1} {rechenart} {zahl2} = {zahl1+zahl2}")
-#Subtraktion
-elif rechenart == "-" ():
-    print(f"{zahl1} {rechenart} {zahl2} = {zahl1-zahl2}")
-#Multiplikation
-elif rechenart == "*" ():
-    print(f"{zahl1} {rechenart} {zahl2} = {zahl1*zahl2}")
-#Division
-elif rechenart == "/" ():
-    print(f"{zahl1} {rechenart} {zahl2} = {zahl1/zahl2}")
-#Nicht möglich
-else:
-    print("Keine gültige Antwort.")
+    #Rechnung
+    if rechenart == '+':
+        print(f'{zahl1} {rechenart} {zahl2} = {zahl1+zahl2}')
+    elif rechenart == '-':
+        print(f'{zahl1} {rechenart} {zahl2} = {zahl1-zahl2}')
+    elif rechenart == '*':
+        print(f'{zahl1} {rechenart} {zahl2} = {zahl1*zahl2}')
+    elif rechenart == '/':
+        print(f'{zahl1} {rechenart} {zahl2} = {zahl1/zahl2}')
+    else:
+        print('Keine gültige Antwort.')
+
+    #Fortsetzung
+    Fortsetzung = str(input('Möchtest du nochmal rechnen?:'))
+
+    if Fortsetzung.lower == "nein":
+        break
+print("Auf Wiedersehen!")
